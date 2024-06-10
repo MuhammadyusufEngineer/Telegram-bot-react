377ebb3babdc8dbf777847f792b2d93834a1a1ef
Muhammadyusuf Engineer <mrimofficial7@gmail.com>
2024-06-10 10:56:21 +0500

    create project, react, tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-center text-7xl text-blue-500 font-mono">Hello there!</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/db.js
@@ -0,0 +1,34 @@
+export function getData() {
+  return [
+    {
+      id: 1,
+      title: 'Javascript',
+      price: 191,
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.worldnomads.com%2Ftravel-safety%2Feurope%2Fswitzerland%2Fmountain-safety-switzerland&psig=AOvVaw3Vxi6eskbYrg_J8RMDbQsy&ust=1718024136300000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKCJm-XIzoYDFQAAAAAdAAAAABAE'
+    },
+    {
+      id: 2,
+      title: 'Typescript',
+      price: 123,
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.totaltypescript.com%2F&psig=AOvVaw1GiTMJ1_Y10L6-tTw_gJ2B&ust=1718024230388000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIixs5HJzoYDFQAAAAAdAAAAABAE'
+    },
+    {
+      id: 3,
+      title: 'Vue.js',
+      price: 987,
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fplainenglish.io%2Fblog%2Fvue-js-vapor-mode-and-other-roadmap-updates-from-vue-amsterdam-2023&psig=AOvVaw3JIhX7L5JoJrkmVWx6dkQE&ust=1718024274470000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPC6jqvJzoYDFQAAAAAdAAAAABAE'
+    },
+    {
+      id: 4,
+      title: 'Nuxt.js',
+      price: 684,
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKHF4JdcF4JA&psig=AOvVaw2JJ4Zzxl5Wao35sT6ka5eq&ust=1718024331746000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOD5l8LJzoYDFQAAAAAdAAAAABAS'
+    },
+    {
+      id: 5,
+      title: 'Node.js',
+      price: 968,
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdevelopers.redhat.com%2Fblog%2F2021%2F02%2F10%2Fadd-standardized-support-information-to-your-node-js-modules&psig=AOvVaw3IRufHlB2sU8FoE5IhKgIn&ust=1718024388881000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMCgg93JzoYDFQAAAAAdAAAAABAb'
+    },
+  ]
+}
