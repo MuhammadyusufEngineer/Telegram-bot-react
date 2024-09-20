4f5a3e5391a25330570512928047e7d48af5c54c
Muhammadyusuf Engineer <mrimofficial7@gmail.com>
2024-09-20 07:40:41 +0500

    i don't know

--- src/constants/db.js
+++ src/constants/db.js
@@ -2,15 +2,15 @@ import axios from 'axios'
 
 async function getData() {
   try {
-    const response = await axios.get('https://muhammadyusufengineer.pythonanywhere.com/api/categories/', {
-      headers: {'Content-Type': 'application/json'},
+    const response = await axios.get('http://127.0.0.1:8000/api/categories/', {
+      headers: { 'Content-Type': 'application/json' },
       withCredentials: true
     })
     return response.data
   } catch (error) {
-    console.error('Failed to fetch data:', error.response ? error.response.data: error.message);
+    console.error('Failed to fetch data:', error.response ? error.response.data : error.message);
     return []; // Return an empty array or handle the error as needed
   }
 }
 
-export default getData
+export default getData 
